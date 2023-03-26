5c238056622eacf4e41fc2f14605dd477914ec10
TamirlanNurbulatuly <t.nurbulatuly@gmail.com>
2023-03-26 18:45:15 +0600

    creating superuser, models

--- Basta/course/models.py
+++ Basta/course/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return self.title
