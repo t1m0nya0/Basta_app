e7732ada4ffa6f8d7f29b72427b68f836d94b2bd
TamirlanNurbulatuly <t.nurbulatuly@gmail.com>
2023-03-27 23:33:51 +0600

    +2 html file extends by base.html, update: def learn(reading db)

--- Basta/course/views.py
+++ Basta/course/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from .models import *
 
 # Create your views here.
 
 
 """class CourseHome(ListView):
     template_name = 'course/index.html'"""
-
+menu = ['Catalog', 'Learning']
 
 def learn(request):
-    return render(request, 'course/index.html')
+    context = {
+        'menu': menu,
+        'courses': Course.objects.all(),
+        'title': 'My learn'
+    }
+    return render(request, 'course/index.html', context=context)
 
 
 def catalog(request):
