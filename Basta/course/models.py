6073fbf271f6d0a262a79e2f7ccb366b215f62b8
TamirlanNurbulatuly <t.nurbulatuly@gmail.com>
2023-03-27 13:16:45 +0600

    add: - in urls.py debugging for image - update models - in settings.py MediaRoot MediaURL

--- Basta/course/models.py
+++ Basta/course/models.py
@@ -4,9 +4,10 @@ from django.db import models
 # Create your models here.
 class Course(models.Model):
     title = models.CharField(max_length=255)
-    description = models.TextField()
-    start_date = models.DateField()
-    end_date = models.DateField()
+    description = models.TextField(blank=True)
+    start_date = models.DateField(auto_now_add=True)
+    end_date = models.DateField(auto_now=True)
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
 
     def __str__(self):
         return self.title
